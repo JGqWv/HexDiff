44c5f9f142a1ce10a333110ca2be228df7a74362
Corcon <corconqwerty@gmail.com>
2025-07-11 10:54:46 +0500

    changes in models, controllers

--- HexDiffNew/Controllers/DiffController.cs
+++ HexDiffNew/Controllers/DiffController.cs
@@ -1,5 +1,6 @@
 ﻿using HexDiffNew.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.ObjectModel;
 
 namespace HexDiffNew.Controllers
 {
@@ -17,15 +18,43 @@ namespace HexDiffNew.Controllers
         //    var hexFile2 = new HexFileModel { FileName = file2.FileName, DataBytes = ReadFile(file2) };
 
         //    var differences = FindDifferences(hexFile1.DataBytes, hexFile2.DataBytes);
-            
+
         //    return View("Results", differences);
         //}
 
-        private byte[] ReadFile(IFormFile file)
+        [HttpPost]
+        public IActionResult Compare(IFormFile file1, IFormFile file2)
         {
+            var hexFile1 = new HexFileModel { FileName = file1.FileName, HexDataCollection = ReadFileToCollection(file1) };
+            var hexFile2 = new HexFileModel { FileName = file2.FileName, HexDataCollection = ReadFileToCollection(file2) };
+
+            var differences = FindDifferences(hexFile1.HexDataCollection, hexFile2.HexDataCollection);
+
+            return View("Results", differences);
+        }
+
+        //private byte[] ReadFile(IFormFile file)
+        //{
+        //    using var stream = new MemoryStream();
+        //    file.CopyTo(stream);
+        //    return stream.ToArray();
+        //}
+
+
+        private Collection<HexData> ReadFileToCollection(IFormFile file)
+        {
+
             using var stream = new MemoryStream();
+
             file.CopyTo(stream);
-            return stream.ToArray();
+            uint index = 0;
+            Collection<HexData> hexDatas = new Collection<HexData>();
+            foreach (byte item in stream.ToArray()) /// maybe we dont need second convertion
+            {
+                hexDatas.Add(new HexData { Addr = index, DataBytes = item });
+                index++;
+            }
+            return hexDatas;
         }
 
         /// <summary>
@@ -34,19 +63,37 @@ namespace HexDiffNew.Controllers
         /// <param name="file1"></param>
         /// <param name="file2"></param>
         /// <returns></returns>
-        private List<HexDifference> FindDifferences(byte[] file1, byte[] file2)
+        //private List<HexDifference> FindDifferences(byte[] file1, byte[] file2)
+        //{
+        //    var differences = new List<HexDifference>();
+        //    int minLength = Math.Min(file1.Length, file2.Length);
+
+        //    for (int i = 0; i < minLength; i++)
+        //    {
+        //        if (file1[i] != file2[i])
+        //            differences.Add(new HexDifference
+        //            {
+        //                Offset = i,
+        //                File1Byte = file1[i],
+        //                File2Byte = file2[i]
+        //            });
+        //    }
+
+        //    return differences;
+        //}
+        private List<HexDifference> FindDifferences(Collection<HexData> file1, Collection<HexData> file2)
         {
             var differences = new List<HexDifference>();
-            int minLength = Math.Min(file1.Length, file2.Length);
+            int minLength = Math.Min(file1.Count, file2.Count);
 
             for (int i = 0; i < minLength; i++)
             {
-                if (file1[i] != file2[i])
+                if (file1.ElementAt<HexData>(i).DataBytes != file2.ElementAt<HexData>(i).DataBytes)
                     differences.Add(new HexDifference
                     {
                         Offset = i,
-                        File1Byte = file1[i],
-                        File2Byte = file2[i]
+                        File1Byte = file1.ElementAt<HexData>(i).DataBytes,
+                        File2Byte = file2.ElementAt<HexData>(i).DataBytes
                     });
             }
 
--- HexDiffNew/Controllers/HexFileController.cs
+++ HexDiffNew/Controllers/HexFileController.cs
@@ -21,14 +21,26 @@ namespace HexDiffNew.Controllers
             return View("Results", hexFile1);
         }
 
+        //[HttpPost]
+        //public IActionResult Save(HexDataModel HexFile)
+        //{
+        //}
+
+        //[HttpPost]
+        //public IActionResult Edit(HexData hexData)
+        //{
+           
+        //}
+
         private Collection<HexData> ReadFileToCollection(IFormFile file)
         {
            
             using var stream = new MemoryStream();
+
            file.CopyTo(stream);
             uint index = 0;
             Collection<HexData> hexDatas = new Collection<HexData>();
-            foreach (byte item in stream.ToArray())
+            foreach (byte item in stream.ToArray()) /// maybe we dont need second convertion
             {
                 hexDatas.Add(new HexData {Addr=index, DataBytes  = item});
                 index++;
